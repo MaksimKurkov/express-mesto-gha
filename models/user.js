d7866cc1df320a134647c80b53ff8665af433b9e
Maksim Kurkov <angellone@yandex.ru>
2023-08-27 18:57:55 +0300

    Тест ошибок ПР13

--- models/user.js
+++ models/user.js
@@ -3,19 +3,19 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    default: 'Жак-Ив Кусто',
+    required: true,
     minlength: [2, 'Минимальная длина поля 2 символа'],
     maxlength: [30, 'Максимальная длина поля 30 символов'],
   },
   about: {
     type: String,
-    default: 'Исследователь',
+    required: true,
     minlength: [2, 'Минимальная длина поля 2 символа'],
     maxlength: [30, 'Максимальная длина поля 30 символов'],
   },
   avatar: {
     type: String,
-    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    required: true,
   },
 });
 
