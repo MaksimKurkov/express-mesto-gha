fe64b82534c8adb82cfe737b3e954a78d328932c
Maksim Kurkov <angellone@yandex.ru>
2023-09-06 02:32:16 +0300

    Проверка автотеста ПР14

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,13 +3,15 @@ const AuthError = require('../errors/authorized-error');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return next(new AuthError('Необходима авторизация'));
+  }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, 'secretKey');
   } catch (err) {
-    return next(new AuthError('Необходима авторизация'));
+    return next(new AuthError('ошибка тут'));
   }
   req.user = payload;
   return next();
